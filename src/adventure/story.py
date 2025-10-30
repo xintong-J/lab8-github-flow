de30f4456fa4c928b4ec6983d1153165422d8796
xintong-JIANG <231744781+xintong-J@users.noreply.github.com>
2025-10-29 23:57:12 -0400

    Updates (#2)
    * style in different colors and consistently.
    
    * second edit according to the PR.

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,6 +1,10 @@
 from adventure.utils import read_events_from_file
 import random
+from rich import print
+from rich.console import Console
+from rich.text import Text
 
+console = Console()
 default_message = "You stand still, unsure what to do. The forest swallows you."
 
 def step(choice: str, events):
@@ -19,14 +23,30 @@ def left_path(event):
 def right_path(event):
     return "You walk right. " + event
 
+def sstyle(text, style):
+    console.print(text, style=style)
+def cstyle():
+    otext = Text()
+    otext.append("Which direction do you choose?", style="bold blue")
+    otext.append("(left/right/exit)", style = "italic magenta")
+    otext.append(": ", style="italic black")
+    console.print(otext)
+
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    sstyle("You wake up in a dark forest. You can go left or right.", "italic red")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
-        choice = choice.strip().lower()
+        cstyle()
+        choice = input().strip().lower()
         if choice == 'exit':
+            sstyle("Exit!!!", "italic green")
             break
-        
+        result = step(choice, events)
+        if "left" in result.lower():
+            sstyle(result, "yellow")
+        elif "right" in result.lower():
+            sstyle(result, "purple4")
+        else:
+            sstyle(result,"cyan")
         print(step(choice, events))
